a062121880f8c71e6ba5a1470e531d237ae431e9
Stefan Pflugbeil <consulting.pflugbeil@gmail.com>
2017-11-19 09:14:07 +0100

    „init

---
+++ HalloWeb/HalloWeb/Controllers/FahrtenbuchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalloWeb.Controllers
+{
+  public class FahrtenbuchController : Controller
+  {
+    public string Index()
+    {
+      return "Fahrtenbuch";
+    }
+  }
+}
---
+++ HalloWeb/HalloWeb/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        // http://localhost:5000/HelloWorld/Welcome?name=Rick&numtimes=4
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+//        public string Welcome()
+//        {
+//            return "This is the Welcome action method...";
+//        }
+    }
+}
